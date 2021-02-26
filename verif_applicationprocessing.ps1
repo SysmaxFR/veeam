4b6f4b4838e54946f7c8dd36e29773d1fde7c0b6
SysmaxFR <68464968+SysmaxFR@users.noreply.github.com>
2021-02-26 18:21:27 +0100

    Create verif_applicationprocessing.ps1

---
+++ verif_applicationprocessing.ps1
@@ -0,0 +1,21 @@
+Add-PSSnapin VeeamPSSnapin
+
+Write-Host "Search in progress ..."
+$BackupJob = Get-VBRJob | Where {($_.JobType -eq "Backup") -and ($_.VssOptions.VssSnapshotOptions.enabled -eq $true) -and ($_.VssOptions.VssSnapshotOptions.ApplicationProcessingEnabled -eq $true)}
+Write-Host "Search ok"
+
+Foreach ($VM in $BackupJob)
+{
+    $VM | Select @{ N="VM Name"; E={$VM.GetObjectsInJob().Name} }, @{ N="JobName"; E={$VM.GetJobDisplayName()} }
+}
+
+#
+# Search in progress ...
+# Search ok
+#
+# VM Name                        JobName                          
+# -------                        -------                              
+# WIN-BCKP-01                    Backup Job 03 - WIN-BCKP-01      
+# {WIN-RDGATEWAY-02, WIN-RDS-02} Backup Job 06 - RDS-00              
+# WIN-AD-01                      Backup Job 13 - WIN-AD-01
+#
